f869a577701e549d560169460b1408e3715850c7
syogo-user <s.photo.trumpet.005@gmail.com>
2023-09-16 19:49:35 +0900

    Initial Commit

---
+++ Fructus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fructus
+//
+//  Created by 小野寺祥吾 on 2023/09/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by 小野寺祥吾 on 2023/09/16.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
